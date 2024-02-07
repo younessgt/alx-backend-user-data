b800a530369d229cd9498263eb63f2c853a0da6b
younessgt <youness.merzak123@gmail.com>
2024-02-07 18:36:21 +0100

    task3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,8 @@
 from typing import List
 import re
 import logging
-
+import mysql.connector
+import os
 PII_FIELDS = ("email", "phone", "ssn", "password", "name")
 
 
@@ -46,3 +47,21 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
     logger.propagate = False
     return logger
+
+
+def get_db() -> mysql.connector.connect:
+    """returns a connector to the database"""
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
+
+    config = {
+            'user': username,
+            'password': password,
+            'host': host,
+            'database': db_name
+    }
+    connection = mysql.connector.connect(**config)
+    return connection
+    # the connection is closed in the 3-main.py file
