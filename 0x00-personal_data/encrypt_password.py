caba31707cd6bccce317be8d503af4e855780050
younessgt <youness.merzak123@gmail.com>
2024-02-07 20:21:01 +0100

    task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,14 @@ def hash_password(password: str) -> bytes:
     str_byte = password.encode('utf-8')
     hashed = bcrypt.hashpw(str_byte, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checking if the provided password
+    matches the hashed password"""
+
+    str_byte = password.encode('utf-8')
+    if bcrypt.checkpw(str_byte, hashed_password):
+        return True
+    else:
+        return False
