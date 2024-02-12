cb72235457a9ffc878f12fe36b285914222a85f4
younessgt <youness.merzak123@gmail.com>
2024-02-12 18:08:25 +0100

    task6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,6 +18,9 @@ AUTH = getenv("AUTH_TYPE")
 if AUTH == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
