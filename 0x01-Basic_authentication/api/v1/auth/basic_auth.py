09f76b46d2ba9107c6d2b3dc1f9f496ee673d8ed
younessgt <youness.merzak123@gmail.com>
2024-02-12 18:49:22 +0100

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ script containig BasicAuth class"""
 from api.v1.auth.auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """ empty class"""
@@ -20,3 +20,23 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        """returns the decoded value of a
+        Base64 string base64_authorization_header """
+
+        if (
+            base64_authorization_header is None or
+            not isinstance(base64_authorization_header, str)
+           ):
+            return None
+
+        try:
+            decoded64 = base64.b64decode(base64_authorization_header)
+
+            return decoded64.decode("utf-8")
+        except Exception:
+            return None
